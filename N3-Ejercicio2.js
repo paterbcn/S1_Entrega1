ce4dd3ecfd10880ce7bfb2456e0e485090f5615d
pater <agustinpaternoster@gmail.com>
2023-02-23 16:42:14 +0100

    "nivel 3 ejercicio 2"

---
+++ N3-Ejercicio2.js
@@ -0,0 +1,4 @@
+/*Crea una funció anònima autoinvocable igualada a una variable que mostri per 
+consola el nom de l'usuari/ària a rebut com a paràmetre.*/
+
+var usuario =(function (nom){console.log(`Nombre usuario: ${nom}`)})("agustin");
